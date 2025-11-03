a50a375af2ef1a2c62b910691bd6e0b665b3709e
CDNGLM <ismaeldk7834@gmail.com>
2025-11-03 16:19:53 -0500

    Update map.py

--- Video Juego/map.py
+++ Video Juego/map.py
@@ -5,8 +5,8 @@ from character import Character
 class Ground (pygame.sprite.Sprite):
     def __init__(self,x, y, withd, hight):
         super().__init__()
-        self.image = pygame.Surface((withd, hight))
-        self.image.fill ((128, 64, 0))
+        self.image = pygame.image.load("ground.png")
+        self.image = pygame.transform.scale(self.image, (withd, hight))
         self.rect = self.image.get_rect(topleft= (x, y))
 
 
@@ -14,4 +14,5 @@ class Background:
     def __init__(self):
         self.background = pygame.image.load("fondo.png")
         self.background = pygame.transform.scale(self.background, (constantes.WITH_SCREEN, constantes.HIGH_SCREEN))
+
         
