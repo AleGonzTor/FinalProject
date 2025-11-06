33b1ea69407cdbf17082f22b387dda30b8eb40a3
Ayelen11 <fmichcapacchua@gmail.com>
2025-11-06 01:58:56 -0500

    Add files via upload

---
+++ Practices/Flor/nivel_1_decoraciones.py
@@ -0,0 +1,9 @@
+from decoration import Decoration
+decoraciones_1=[
+    Decoration(100,200,"Sprites/arbol.png",layer=0),
+    Decoration(300,180,"Sprites/arbol.png",layer=0),
+    Decoration(200,30,"Sprites/arbusto.png",layer=0),
+    Decoration(270,180,"Sprites/hongo.png",layer=1),
+    Decoration(30,30,"Sprites/nube.png",layer=0)
+]
+decoraciones_1[1].escala(80,100)
