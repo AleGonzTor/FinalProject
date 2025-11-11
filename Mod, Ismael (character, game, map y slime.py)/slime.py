92554cb6871bd3aeb8a62704fd8e95384817609e
AleGonzTor <alegonztor@unsa.edu.pe>
2025-11-11 09:54:20 -0500

    Deleted unnecesary folder and made spike work.

--- Mod, Ismael (character, game, map y slime.py)/slime.py
+++
@@ -1,9 +0,0 @@
-import pygame
-from constants import *
-from obstacle import *
-
-class Slime(Obstacle):
-    def update(self):
-        self.rect.x += self.speed
-        if self.rect.right >= WIDTH or self.rect.left <= 0:
-            self.speed *= -1
