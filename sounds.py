e94d2f063bdb49f012d26eedac8949b3994acc0f
Ayelen11 <fmichcapacchua@gmail.com>
2025-11-13 06:13:10 -0500

    Create SoundManager class for sound handling
    Refactor sound management into SoundManager class and add new sounds.

--- sounds.py
+++ sounds.py
@@ -1,15 +1,25 @@
 import pygame
 
 
-pygame.mixer.init()  
+class SoundManager:
+    def __init__(self):
+        pygame.mixer.init()  
 
-sounds = {
-    "walk": pygame.mixer.Sound("./Sounds/walk1.wav"),
-    "jump": pygame.mixer.Sound("./Sounds/Jump.wav"),
-    "respawn": pygame.mixer.Sound("./Sounds/lose.wav"),
-    "hit": pygame.mixer.Sound("./Sounds/hurt5.wav"),
+        self.sounds = {
+         "walk": pygame.mixer.Sound("./Sounds/walk1.wav"),
+         "jump": pygame.mixer.Sound("./Sounds/Jump.wav"),
+         "respawn": pygame.mixer.Sound("./Sounds/lose.wav"),
+         "hit": pygame.mixer.Sound("./Sounds/hurt5.wav"),
+         "bounce": pygame.mixer.Sound("./Sounds/trampoline.wav"),
+         "hit_slime": pygame.mixer.Sound("./Sounds/hit2.wav")
+           }
+        pygame.mixer.music.load("./Sounds/music.mp3")
+        pygame.mixer.music.play(-1)  
+        pygame.mixer.music.set_volume(0.5) 
 
-    }
-pygame.mixer.music.load("./Sounds/music.mp3")
-pygame.mixer.music.play(-1)  
-pygame.mixer.music.set_volume(0.5) 
+    def play(self, name):
+        if name in self.sounds:
+            self.sounds[name].play()
+
+sound_manager = SoundManager()
+
