7d2ecb024c463dfcfd6f7f9a4b61c7275936167e
CDNGLM <ismaeldk7834@gmail.com>
2025-11-19 11:54:35 -0500

    Update jetpack.py

--- jetpack.py
+++ jetpack.py
@@ -2,32 +2,26 @@ from obj import *
 from constants import *
 
 class Jetpack(Object):
-    def __init__(self, x, y, pic="Jetpack", duration = 5, respawn = 10):
+    def __init__(self, x, y, pic="Jetpack", duration = 2, respawn = 10):
         super().__init__(x, y, pic)
         self.duration = duration
         self.time_respawn = respawn
-        self.original_pos = (x, y)
+        self.original_pos = (x * 18, y *18)
         self.visible = True
         self.timer = 0
         
     def update(self, dt):
-        if self.visible:
-            self.timer += dt
-            if self.timer >= self.duration:
-                self.hide()
-        
-        else:
+        if self.visible == False:
             self.timer += dt
             if self.timer >= self.time_respawn:
                 self.respawn()
                 
-    def hide(self):
+    
+    def pick (self):
         self.visible = False
         self.timer = 0
         self.rect.topleft = (-10000, -10000)
-    
-    def pick (self):
-        self.hide()
+        
     
     def respawn(self):
         self.visible = True
